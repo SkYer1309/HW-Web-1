9b79939ffbae5bacdf4b8c02937a1be5f47fc1a7
SkYer13 <sky13net84@gmail.com>
2026-09-15 10:48:23 +0300

    Закончено ДЗ: Продолжение знакомства с SQL

--- src/main/java/ru/hogwarts/school/repository/StudentRepository.java
+++ src/main/java/ru/hogwarts/school/repository/StudentRepository.java
@@ -1,14 +1,27 @@
 package ru.hogwarts.school.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.hogwarts.school.model.Student;
 
 import java.util.Collection;
+import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
     Collection<Student> findByAge(int age);
 
-    // Поиск студентов в диапазоне возрастов
     Collection<Student> findByAgeBetween(int min, int max);
+
+    // 1. Количество всех студентов
+    @Query("SELECT COUNT(s) FROM Student s")
+    long countAllStudents();
+
+    // 2. Средний возраст студентов
+    @Query("SELECT AVG(s.age) FROM Student s")
+    Double getAverageAge();
+
+    // 3. Пять последних студентов (по убыванию id)
+    @Query(value = "SELECT * FROM student ORDER BY id DESC LIMIT 5", nativeQuery = true)
+    List<Student> findLastFiveStudents();
 }
