768fada94aeb60d168e951016b33d942bdf4c6f4
SkYer13 <sky13net84@gmail.com>
2026-09-17 15:35:56 +0300

    Закончено ДЗ: Управление схемами баз данных 2

--- src/main/java/ru/hogwarts/school/controller/FacultyController.java
+++ src/main/java/ru/hogwarts/school/controller/FacultyController.java
@@ -16,48 +16,39 @@ public class FacultyController {
     public FacultyController(FacultyService facultyService) {
         this.facultyService = facultyService;
     }
-
     @PostMapping
     public Long createFaculty(@RequestBody Faculty faculty) {
         return facultyService.addFaculty(faculty).getId();
     }
-
     @GetMapping("/{id}")
     public Faculty getFaculty(@PathVariable Long id) {
         return facultyService.getFaculty(id);
     }
-
     @GetMapping
     public Collection<Faculty> getAllFaculties() {
         return facultyService.getAllFaculties();
     }
-
     @PutMapping
     public Faculty updateFaculty(@RequestBody Faculty faculty) {
         return facultyService.editFaculty(faculty);
     }
-
     @DeleteMapping("/{id}")
     public void deleteFaculty(@PathVariable Long id) {
         facultyService.deleteFaculty(id);
     }
-
     @GetMapping("/filter/color")
     public Collection<Faculty> getFacultiesByColor(@RequestParam String color) {
         return facultyService.findByColor(color);
     }
-
     @GetMapping("/filter/name")
     public Collection<Faculty> getFacultiesByName(@RequestParam String name) {
         return facultyService.findByName(name);
     }
-
     // Новый эндпоинт: поиск факультета по тексту (регистронезависимый)
     @GetMapping("/filter/text")
     public Collection<Faculty> getFacultiesByText(@RequestParam String text) {
         return facultyService.findByText(text);
     }
-
     // Новый эндпоинт: получить студентов факультета
     @GetMapping("/{id}/students")
     public Collection<Student> getFacultyStudents(@PathVariable Long id) {
